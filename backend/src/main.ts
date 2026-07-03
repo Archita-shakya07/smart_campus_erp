46bdf7351be8fd496db60441b4ffdb86b396270c
Archita-shakya07 <architashakya80@gmail.com>
2026-07-03 16:30:02 +0530

    Fix CORS configuration for production deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,8 +8,12 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-  app.enableCors({ origin: 'http://localhost:3000', credentials: true });
-
+  app.enableCors({
+  origin: [
+    'http://localhost:3000',
+    'https://smart-campus-erp-brown.vercel.app'
+  ],
+});
   await app.listen(process.env.PORT ?? 5000);
   console.log(`🚀 Server running on http://localhost:${process.env.PORT ?? 5000}`);
 }
